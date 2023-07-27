1dc65dbbd914fbd90aa174e0b579f76c067a1eac
IgorBaratta <igorbaratta@gmail.com>
2023-07-27 12:21:08 +0100

    test

--- main.cpp
+++ main.cpp
@@ -4,42 +4,82 @@
 #include <array>
 #include <chrono>
 
+// This block enables to compile the code with and without the likwid header in place
+// #ifdef LIKWID_PERFMON
+// #include <likwid-marker.h>
+// #else
+// #define LIKWID_MARKER_INIT
+// #define LIKWID_MARKER_THREADINIT
+// #define LIKWID_MARKER_REGISTER(regionTag)
+// #define LIKWID_MARKER_START(regionTag)
+// #define LIKWID_MARKER_STOP(regionTag)
+// #define LIKWID_MARKER_CLOSE
+// #endif
+
 #include "tm.hpp"
 #include "tensor.hpp"
 
+#ifdef DEGREE
+constexpr int p = DEGREE;
+#else
+constexpr int p = 4;
+#endif
+
 using T = double;
 
-int main()
+int main(int argc, char **argv)
 {
 
-    // read input from terminal
-    constexpr int p = 12;
+    // // Register the region "kernel" with LIKWID
+    // LIKWID_MARKER_INIT;
+    // LIKWID_MARKER_REGISTER("kernel-naive");
+    // LIKWID_MARKER_REGISTER("kernel");
+    // LIKWID_MARKER_THREADINIT;
 
-    Tensor<T, p + 1, p + 1, p + 1> u;
-    Tensor<T, p + 1, p + 1, p + 1> w;
-    Matrix<T, p + 1, p + 1> phi;
-
-    phi.fill_random();
-    u.fill_random();
+    std::vector<T> phi_data((p + 1) * (p + 1), 0);
     {
-        auto start = std::chrono::high_resolution_clock::now();
-        for (int i = 0; i < 100000; ++i)
-            tensor_contraction(phi, u, w);
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end - start;
-        std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
+        Matrix<T, p + 1, p + 1> phi(phi_data.data());
+
+        // Fill the matrices with random values
+        phi.fill_random();
+        T p0 = phi(0, 0);
+
+        std::cout << p0 << std::endl;
+
+        constexpr int nd = 1e8;
+        constexpr int nd_cell = (p + 1) * (p + 1) * (p + 1);
+        constexpr int num_cells = nd / nd_cell;
+
+        // allocate data for Tensor (P+1)*(P+1)*(P+1)
+        std::vector<T> u_data(num_cells * (p + 1) * (p + 1) * (p + 1));
+        std::vector<T> w_data(num_cells * (p + 1) * (p + 1) * (p + 1));
+
+        // // Compute the tensor contraction
+        // {
+        //     LIKWID_MARKER_START("kernel-naive");
+        //     for (int i = 0; i < num_cells; ++i)
+        //     {
+        //         T *phi = phi_data.data();
+        //         T *u = u_data.data() + i * (p + 1) * (p + 1) * (p + 1);
+        //         T *w = w_data.data() + i * (p + 1) * (p + 1) * (p + 1);
+        //         tensor_contraction_naive<T, p + 1, p + 1>(phi, u, w);
+        //     }
+        //     LIKWID_MARKER_STOP("kernel-naive");
+        // }
+
+        // {
+        //     LIKWID_MARKER_START("kernel");
+        //     for (int i = 0; i < num_cells; ++i)
+        //     {
+        //         T *phi = phi_data.data();
+        //         T *u = u_data.data() + i * (p + 1) * (p + 1) * (p + 1);
+        //         T *w = w_data.data() + i * (p + 1) * (p + 1) * (p + 1);
+        //         tensor_contraction<T, p + 1, p + 1>(phi, u, w);
+        //     }
+        //     LIKWID_MARKER_STOP("kernel");
+        // }
+
+        // LIKWID_MARKER_CLOSE;
     }
-
-    {
-        auto start = std::chrono::high_resolution_clock::now();
-        for (int i = 0; i < 100000; ++i)
-            tensor_contraction_naive(phi, u, w);
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end - start;
-        std::cout << "elapsed time naive: " << elapsed_seconds.count() << "s\n";
-
-        std::cout << phi[0] << std::endl;
-    }
-
     return 0;
 }
