e8cc6f2d1d0f58bf378b05cc7f105f01ff3cb292
IgorBaratta <igorbaratta@gmail.com>
2023-08-03 17:44:54 +0100

    remove unused code

--- src/tm.hpp
+++ src/tm.hpp
@@ -3,11 +3,20 @@
 
 #define restrict __restrict__
 
-// typedef float float4 __attribute__((ext_vector_type(4)));
-// typedef float float8 __attribute__((ext_vector_type(8)));
-// typedef float float16 __attribute__((ext_vector_type(16)));
-// typedef double double4 __attribute__((ext_vector_type(4)));
-// typedef double double8 __attribute__((ext_vector_type(8)));
+// check if compiler is clang
+#if defined(__clang__)
+typedef float float4 __attribute__((ext_vector_type(4)));
+typedef float float8 __attribute__((ext_vector_type(8)));
+typedef float float16 __attribute__((ext_vector_type(16)));
+typedef double double4 __attribute__((ext_vector_type(4)));
+typedef double double8 __attribute__((ext_vector_type(8)));
+#elif defined(__GNUC__) || defined(__GNUG__)
+typedef float float4 __attribute__((vector_size(4 * sizeof(float))));
+typedef float float8 __attribute__((vector_size(8 * sizeof(float))));
+typedef float float16 __attribute__((vector_size(16 * sizeof(float))));
+typedef double double4 __attribute__((vector_size(4 * sizeof(double))));
+typedef double double8 __attribute__((vector_size(8 * sizeof(double))));
+#endif
 
 namespace linalg
 {
@@ -209,103 +218,3 @@ namespace linalg
     }
 
 } // namespace
-
-// // --------------------------------------------------------------------//
-// /// Compute the tensor contraction C[a,b,c] <- Phi[a, k] U[k, b, c]
-// /// @param[in] Phi tensor of shape (Na, Nk)
-// /// @param[in] U tensor of shape (Nk, Nb, Nc)
-// /// @param[out] C tensor of shape (Na, Nb, Nc)
-// template <typename T, std::size_t Nq, std::size_t Nd, TensorLayout layout = default_layout>
-// void tensor_contraction_naive(T *restrict phi, T *restrict u, T *restrict w)
-// {
-//     Matrix<T, Nd, Nq> PHI(phi);
-//     Tensor<T, Nd, Nd, Nd, layout> U(u);
-//     Tensor<T, Nq, Nd, Nd, layout> W(w);
-
-//     T temp0[Nq * Nd * Nd];
-//     T temp1[Nq * Nd * Nd];
-
-//     {
-//         Tensor<T, Nq, Nd, Nd> TEMP0(temp0);
-//         Tensor<T, Nq, Nd, Nd> TEMP1(temp1);
-
-//         // Temp0[iq0, i1, i2] <- Phi[iq0, i0] U[i0, i1, i2]
-//         for (std::size_t iq0 = 0; iq0 < Nq; iq0++)
-//             for (std::size_t i1 = 0; i1 < Nd; i1++)
-//                 for (std::size_t i2 = 0; i2 < Nd; i2++)
-//                     for (std::size_t i0 = 0; i0 < Nd; i0++)
-//                         TEMP0(iq0, i1, i2) += PHI(iq0, i0) * U(i0, i1, i2);
-
-//         // Temp1[iq0, iq1, i2] <- Phi[iq1, i1] Temp0[iq1, i1, i2]
-//         for (std::size_t iq0 = 0; iq0 < Nq; iq0++)
-//             for (std::size_t iq1 = 0; iq1 < Nq; iq1++)
-//                 for (std::size_t i2 = 0; i2 < Nd; i2++)
-//                     for (std::size_t i1 = 0; i1 < Nd; i1++)
-//                         TEMP1(iq0, iq1, i2) += PHI(iq1, i1) * TEMP0(iq0, i1, i2);
-
-//         // W[iq0, iq1, iq2] <- Phi[iq2, i2] Temp1[iq0, iq1, iq2]
-//         for (std::size_t iq0 = 0; iq0 < Nq; iq0++)
-//             for (std::size_t iq1 = 0; iq1 < Nq; iq1++)
-//                 for (std::size_t iq2 = 0; iq2 < Nq; iq2++)
-//                     for (std::size_t i2 = 0; i2 < Nd; i2++)
-//                         W(iq0, iq1, iq2) += PHI(iq2, i2) * TEMP1(iq0, iq1, i2);
-//     }
-// }
-
-// // --------------------------------------------------------------------//
-// /// Compute the tensor contraction C[a,b,c] <- Phi[a, k] U[k, b, c]
-// /// @param[in] Phi tensor of shape (Na, Nk)
-// /// @param[in] U tensor of shape (Nk, Nb, Nc)
-// /// @param[out] C tensor of shape (Na, Nb, Nc)
-// template <typename T, std::size_t Nq, std::size_t Nd, TensorLayout layout = default_layout>
-// void tensor_contraction(T *restrict phi, T *restrict u, T *restrict w)
-// {
-//     Tensor<T, Nd, Nd, Nd, layout> U(u);
-//     Tensor<T, Nq, Nd, Nd, layout> W(w);
-
-//     {
-//         // ------------------------------------------------------------------
-//         // Contraction index: i0
-//         // Temp0[iq0, i1, i2] <- Phi[iq0, i0] U[i0, i1, i2]
-//         // Can be interpreted as a matrix multiplication
-//         // Temp0[iq0, i1_i2] <- Phi[iq0, i0] U[i0, i1_i2]
-//         // where i1_i2 is the index (i1, i2) flattened
-
-//         T temp0[Nq * (Nd * Nd)] = {0.0};    // Temporary storage for the matrix multiplication
-//         Tensor<T, Nq, Nd, Nd> TEMP0(temp0); // Wrap the temporary storage in a tensor
-//         micro_gemm<T, Nd, Nq, Nd * Nd>(phi, u, temp0);
-
-//         // // ------------------------------------------------------------------
-//         // // Contraction index: i1
-//         // // Temp1[iq0, iq1, i2] <- Phi[iq1, i1] Temp0[iq0, i1, i2]
-//         // // Temp0_T[i1, iq1, i2] <- Temp0[iq0, i1, i2]
-//         // // (jik) <- (ijk)
-//         // // Temp1[iq0, iq1, i2] < -Phi[iq1, i1] Temp0_T[i1, iq0, i2]
-//         T temp0_trans[Nq * Nd * Nd];
-//         Tensor<T, Nq, Nd, Nd, TensorLayout::kij> TEMP0_t(temp0_trans);
-//         TEMP0.template to_layout<TensorLayout::kij>(TEMP0_t); // transpose
-
-//         // // Temp1[iq1, iq0_i2] <- Phi[iq1, i1] Temp0_T[i1, iq0_i2]
-//         // // where iq0_i2 is the index (iq0, i2) flattened
-//         // // Final result is stored in Temp1[iq1, iq0, i2] (jik)
-//         // T temp1[Nq * Nq * Nd];
-//         // Tensor<T, Nq, Nq, Nd, TensorLayout::jik> TEMP1(temp1);
-
-//         // // Can be interpreted as a matrix multiplication
-//         micro_gemm<T, Nd, Nq, Nq * Nd>(phi, temp0_trans, w);
-
-//         // // ------------------------------------------------------------------
-//         // // Contraction index: i2
-//         // // W[iq0, iq1, iq2] <- Phi[iq2, i2] Temp1[iq1, iq0, i2]
-//         // // Temp1_T[i2, iq1, iq0] <- Temp1[iq1, iq0, i2]
-//         // // (kij) <- (jik)
-//         // T temp1_trans[Nq * Nq * Nd];
-//         // Tensor<T, Nq, Nq, Nd, TensorLayout::kij> TEM1_t(temp1_trans);
-//         // TEMP1.template to_layout<TensorLayout::kij>(TEM1_t); // transpose
-
-//         // // W[iq0, iq1, iq2] <- Phi[iq2, i2] Temp1_T[i2, iq1, iq0]
-//         // micro_gemm<T, Nd, Nq, Nq * Nd>(phi, TEM1_t.data_ptr(), w);
-
-//         // W[iq2, iq1, iq0] <- Phi[iq2, i2] Temp1_T[i2, iq1, iq0]
-//     }
-// }
